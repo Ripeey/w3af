14497d7fb3317c7dc3445a70837493c6f4a2e190
Nong Hoang Tu <dmknght@parrotsec.org>
2021-06-21 03:46:31 +0700

    Some syntax improvements

--- w3af/core/controllers/threads/queues.py
+++ w3af/core/controllers/threads/queues.py
@@ -13,6 +13,7 @@ class SimpleQueueWithSize(SimpleQueue):
     def _make_methods(self):
         recv = self._reader.recv
         racquire, rrelease = self._rlock.acquire, self._rlock.release
+
         def get():
             racquire()
             try:
@@ -20,6 +21,7 @@ class SimpleQueueWithSize(SimpleQueue):
             finally:
                 self._qsize -= 1
                 rrelease()
+
         self.get = get
 
         if self._wlock is None:
@@ -28,6 +30,7 @@ class SimpleQueueWithSize(SimpleQueue):
         else:
             send = self._writer.send
             wacquire, wrelease = self._wlock.acquire, self._wlock.release
+
             def put(obj):
                 wacquire()
                 try:
@@ -35,5 +38,5 @@ class SimpleQueueWithSize(SimpleQueue):
                 finally:
                     self._qsize += 1
                     wrelease()
-            self.put = put
 
+            self.put = put
